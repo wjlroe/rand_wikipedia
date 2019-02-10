83300bb3ce4c1baa0a16b1cb8e9faddf63b97c82
William Roe <git@wjlr.org.uk>
2019-02-10 22:02:32 +0000

    Use default rustfmt config

--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,7 @@ use rand::seq::SliceRandom;
 use scraper::{Html, Selector};
 use std::collections::HashSet;
 
-const LIST_ITEMS: &'static str =
-    "#bodyContent > #mw-content-text > div ul > li > a";
+const LIST_ITEMS: &'static str = "#bodyContent > #mw-content-text > div ul > li > a";
 
 pub fn parse_page(page_contents: String) -> Vec<String> {
     let document = Html::parse_document(&page_contents);
@@ -17,10 +16,7 @@ pub fn parse_page(page_contents: String) -> Vec<String> {
         .collect::<Vec<String>>()
 }
 
-pub fn names_excluding(
-    names: &Vec<String>,
-    excluding: &Vec<String>,
-) -> Vec<String> {
+pub fn names_excluding(names: &Vec<String>, excluding: &Vec<String>) -> Vec<String> {
     let a: HashSet<_> = names.iter().cloned().collect();
     let b: HashSet<_> = excluding.iter().cloned().collect();
     a.difference(&b).cloned().collect()
