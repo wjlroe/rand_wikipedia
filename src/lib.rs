e06cbf8f835c54fe28b45931b1f99982217ad3a7
William Roe <git@wjlr.org.uk>
2019-02-10 22:00:21 +0000

    Rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -6,26 +6,27 @@ use scraper::{Html, Selector};
 use std::collections::HashSet;
 
 const LIST_ITEMS: &'static str =
-        "#bodyContent > #mw-content-text > div ul > li > a";
+    "#bodyContent > #mw-content-text > div ul > li > a";
 
 pub fn parse_page(page_contents: String) -> Vec<String> {
-        let document = Html::parse_document(&page_contents);
-        let selector = Selector::parse(LIST_ITEMS).unwrap();
-        document.select(&selector)
-                .map(|element| element.text().collect::<Vec<&str>>().join(" "))
-                .collect::<Vec<String>>()
+    let document = Html::parse_document(&page_contents);
+    let selector = Selector::parse(LIST_ITEMS).unwrap();
+    document
+        .select(&selector)
+        .map(|element| element.text().collect::<Vec<&str>>().join(" "))
+        .collect::<Vec<String>>()
 }
 
 pub fn names_excluding(
-        names: &Vec<String>,
-        excluding: &Vec<String>,
+    names: &Vec<String>,
+    excluding: &Vec<String>,
 ) -> Vec<String> {
-        let a: HashSet<_> = names.iter().cloned().collect();
-        let b: HashSet<_> = excluding.iter().cloned().collect();
-        a.difference(&b).cloned().collect()
+    let a: HashSet<_> = names.iter().cloned().collect();
+    let b: HashSet<_> = excluding.iter().cloned().collect();
+    a.difference(&b).cloned().collect()
 }
 
 pub fn rand_name(names: &Vec<String>) -> String {
-        let mut rng = rand::thread_rng();
-        names.choose(&mut rng).unwrap().clone()
+    let mut rng = rand::thread_rng();
+    names.choose(&mut rng).unwrap().clone()
 }
